d09507999d0ea82b10d8949d07579480cb41be18
M9R7G <44979106+M9R7G@users.noreply.github.com>
2023-06-07 19:17:23 +0200

    Update page_config_dict.py

--- page_config_dict.py
+++ page_config_dict.py
@@ -2,6 +2,6 @@ from PIL import Image
 
 PAGE_CONFIG = {"page_title"            : "DSBXXRT",
             #    "page_icon"             : ":panda_face:",
-               "page_icon"             : Image.open("sources/curiosidades-del-oso-panda-1280x720x80xX.jpg"),
+               "page_icon"             : Image.open("curiosidades-del-oso-panda-1280x720x80xX.jpg"),
                "layout"                : "wide",
                "initial_sidebar_state" : "collapsed"}
